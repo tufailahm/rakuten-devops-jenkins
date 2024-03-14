094767445bcfaa156f1ae0449abd84d4fa0c418c
Mohammad Tufail Ahmed <tufailahmedkhan@gmail.com>
2024-03-14 12:10:00 +0530

    first commit

---
+++ src/main/java/com/training/pms/RakutenDevopsJenkinsApplication.java
@@ -0,0 +1,20 @@
+package com.training.pms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class RakutenDevopsJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RakutenDevopsJenkinsApplication.class, args);
+	}
+
+	@GetMapping
+	public String getMessage() {
+		return "Rakuten India welcomes you";
+	}
+}
